426833bfb0bf541bec2aa81679b043c5e3ad99e1
Swadhin Biswas <144092840+swadhingithub@users.noreply.github.com>
2024-10-20 19:17:40 +0600

    Update settings.py

--- ProjectDev/settings.py
+++ ProjectDev/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%rf3uvjq8_s@xd!7^vqz*26^)38y8e=vjtuv=^tubp-(tzmp&c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 AUTHENTICATION_BACKENDS = [
     
