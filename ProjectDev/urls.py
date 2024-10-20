a40a43465c47211225fa7462cc393c06ac740a65
Swadhin Biswas <144092840+swadhingithub@users.noreply.github.com>
2024-10-20 19:03:37 +0600

    Update urls.py

--- ProjectDev/urls.py
+++ ProjectDev/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include,re_path
-
+from django.shortcuts import redirect
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -45,11 +45,14 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
    authentication_classes=[],
 )
+def redirect_view(request):
+    return redirect('https://swadhin.my.id/')
 
 urlpatterns = [
     path('accounts/login/', admin.site.urls),
     path('api/v1/', include('core.urls')),
     path('api/v1/', include('Students.urls')),
+    path('dev/', redirect_view, name='redirect_view'),
 
   
     path('swagger/<format>/', schema_view.with_ui(cache_timeout=0), name='schema-json'),
